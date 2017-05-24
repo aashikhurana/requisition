bbf38d526aa50c51bd8e06351899a83fbc78a358
aashikhurana <aashi.khurana@gmail.com>
2017-05-24 16:20:47 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -14,10 +14,21 @@ restService.use(bodyParser.json());
 restService.get('/place-order', function(req, res) {
     var speech = "Your Requisition is created with id 123451234 and purchase id is 1234"
     return res.json{
-        speech: speech,
-        displayText: speech,
-        source: 'requisitionbot'
-    };
+                speech:speech,
+                displayText:speech,
+                data:{
+                    google:{
+                        expect_user_response: false,
+                        final_response: {
+                            speech_response: {
+                            text_to_speech: data.message
+                            }
+                        }
+                    }
+                },
+                contextOut:[],
+                source:"requisitionbot"
+            };
 });
 
 
