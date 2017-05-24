72b7861bea4a984c8d2a0e8fffffa31687fafd3a
aashikhurana <aashi.khurana@gmail.com>
2017-05-24 14:49:22 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ restService.use(bodyParser.urlencoded({
 
 restService.use(bodyParser.json());
 
-restService.get('/echo', function(req, res) {
-    var speech = "Your Requisition is created with id 123451234 and purchase id 1234"
+restService.get('/place-order', function(req, res) {
+    var speech = "Your Requisition is created with id 123451234 and purchase id is 1234"
     return res.json({
         speech: speech,
         displayText: speech,
-        source: 'webhook-echo-sample'
+        source: 'requisitionbot'
     });
 });
 
