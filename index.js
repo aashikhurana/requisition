54b4d2ff86094f3312f29eb0ce60b3ed28af7a5f
aashikhurana <aashi.khurana@gmail.com>
2017-05-25 14:54:28 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
     var speech ="Your Requisition Bot Code is 12341234 and purchase Id is 12341234";
+	var user_request=JSON.stringify(req);
+	console.log("user rrequest is of format:"+user_request);
     return res.json({
         speech: speech,
         displayText: speech,
