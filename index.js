34158fac2053fad985d7baed078ded650f905452
aashikhurana <aashi.khurana@gmail.com>
2017-05-25 17:09:29 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -14,11 +14,12 @@ var json_body_parser = bodyParser.json();
 
 restService.post('/echo', json_body_parser, function(req, res) {
 	console.log("Inside Web service call");
+	var speech="There is an error in calling Bot service";
     var user_request=JSON.stringify(req.body.result.action);
 	var order_item=req.body.result.parameters.order_items;
 	var address=req.body.result.parameters.address;
 	if(req.result.action == 'ask'){
-	var speech ="Your Requisition Bot Code is 12341234 and purchase Id is 12341234";
+	speech="Your requisition bot Id is 1234 and purchase id is 12345";
 	console.log("user request is of format:"+user_request);
 	console.log("parameters are: "+ JSON.stringify(order_item) +" "+JSON.stringify(address));
 	}
