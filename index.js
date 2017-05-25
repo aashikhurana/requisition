fb3c95d4055768ccc8c2b882e02febd0933be4f9
aashikhurana <aashi.khurana@gmail.com>
2017-05-25 16:09:19 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 'use strict';
 
 const express = require('express');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 
 const restService = express();
 
-restService.use(bodyParser.urlencoded({
-    extended: true
-}));
+//restService.use(bodyParser.urlencoded({
+    //extended: true
+//}));
 
-restService.use(bodyParser.json());
+//restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
-    var user_request=JSON.stringify(req);
+    var user_request=JSON.stringify(req.body);
 	//if(req.result.action == 'ask'){
 	var speech ="Your Requisition Bot Code is 12341234 and purchase Id is 12341234";
 	console.log("user request is of format:"+user_request);
