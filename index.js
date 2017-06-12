062eeb2587b870ed3aec6a0c98c093a5321b7a06
aashikhurana <aashi.khurana@gmail.com>
2017-06-12 12:36:24 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -216,11 +216,13 @@ soap.createClient(url, function(err, client){
     console.log(requestResult);
 	speech="Thank you for using requisition BOt. Your Id is"+requestResult;
 	}else{
-		console.log(err.message);
+		console.log("There was some error in generating id."+err.message);
 		speech="There was some error in generating id.";
-  });
-	  }else{
-		  console.log(err.message);
+  }
+	  });
+	  }
+	  else{
+		  console.log("There was some error in registering data"+err.message);
 		  speech="There was some error in registering data";
 	  }
 	});
