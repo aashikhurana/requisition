f04d6e6eb224cc57ea9d52f6b8219bbb41eef378
aashikhurana <aashi.khurana@gmail.com>
2017-07-03 13:17:08 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ var json_body_parser = bodyParser.json();
 
 
 
-restService.post('/echo', json_body_parser, function(req, res) {
+restService.post('/mockup', json_body_parser, function(req, res) {
 	console.log("Inside Web service call");
 	
 	
@@ -32,234 +32,48 @@ restService.post('/echo', json_body_parser, function(req, res) {
 	var order_item_1=req.body.result.parameters.order_items1;
 	var address=req.body.result.parameters.order-address;
 	var order_color=req.body.result.parameters.color;
+	var order_yes=req.body.result.parameters.order_yes_no;
 	
-	var item_description="blank";
-    var category_name= "blank";
-    var source_agreement_number=" ";
-    var supplier_item_number=" ";
-    var supplier_contact_name="abcd";
-    var supplier_name= "OD";
-    var supplier_site_name= "OD";
-    var price=" ";
 	var speech="";
 	
-	var requisition_id=" ";
-	var OrderRequestElement=" ";
+	if(order_item==='Solar Panel'){
 	
-var url = 'https://ucf6-fap1297-prc.oracledemos.com/prcPoEditDocumentPurchaseRequest/PurchaseRequestService?WSDL';
-
-	
-	
-	
-	if(user_request == 'place_order'){
-		
-		console.log("user requested"+user_request);
-		
-	if(order_item=='PEN'){
-		console.log("Inside pen")
-		
-		    category_name="Pens and Pencils";
-            source_agreement_number="52172";
-		    supplier_contact_name="Gasol, Jim";
-            supplier_name="Office Depot";
-            supplier_site_name="OD US1";
-            price="3.60";
-			
-		if(order_color=='black'){
-			console.log("Inside black")
-            item_description="Round Stic Ball Point Pen, Fine, Black";
-            supplier_item_number="BIC20131";
-            
-		}else{
-			
-			item_description="Round Stic Ball Point Pen, Fine, Blue";
-            supplier_item_number="BIC20130";
-            }
-				
-		
-	}else if(order_item=='Stapler'){
-		
-	console.log("Inside Stapler");
-	
-	 item_description="Image 1500 Series Desk Accessories, Stapler";
-     category_name= "Desk Supplies";
-     source_agreement_number="52172";
-     supplier_item_number="ELD15681";
-     supplier_contact_name="Gasol, Jim";
-     supplier_name= "Office Depot";
-     supplier_site_name= "OD US1";
-     price="29.95";
-		
-	}else if(order_item=='Organizer'){
-		
-	 item_description="Catch All Organizer, Black";
-     category_name= "Desk Supplies";
-     source_agreement_number="52172";
-     supplier_item_number="ELD16265";
-     supplier_contact_name="Gasol, Jim";
-     supplier_name= "Office Depot";
-     supplier_site_name= "OD US1";
-     price="8.99";
-		
-	}else if(order_item=='Binder Books'){
-		
-		console.log("Inside Binder Books");
-		
-	 item_description="Recycled Presstex Data Binder with Storage Hooks for 9-1/2 inch x 11 inch Sheet Size";
-     category_name= "Notebooks and Binders";
-     source_agreement_number="52172";
-     supplier_item_number="ACC54113";
-     supplier_contact_name="Gasol, Jim";
-     supplier_name= "Office Depot";
-     supplier_site_name= "OD US1";
-     price="6.45";
-		
-	}else if(order_item=='Laminating sheet'){
-		
-		console.log("InsideLaminating sheets");
-		
-	 item_description="Laminating Sheet, 2 mils, Clear, 9 inch x 12 inch";
-     category_name= "Office Supplies";
-     source_agreement_number="52172";
-     supplier_item_number="CLI65001";
-     supplier_contact_name="Gasol, Jim";
-     supplier_name= "Office Depot";
-     supplier_site_name= "OD US1";
-     price="21.99";
-	}else if(order_item=='Planning System'){
-		
-		console.log("Inside Planning system");
-		
-	 item_description="Franklin Covey Planning System, Snap Tab, Champion, Page Size 5-1/2 inch x 8-1/2 inch, Black";
-     category_name= "Office Supplies";
-     source_agreement_number="52176";
-     supplier_item_number="AAG56703";
-     supplier_contact_name="Jhee, Abe";
-     supplier_name= "Staples";
-     supplier_site_name= "Staples US1";
-     price="65.99";
-	}else if(order_item=='Paper'){
-	
-	  console.log("Inside Paper");
-	  item_description="Photocopy Paper Letter Light Blue";
-     category_name= "Copy Paper";
-     source_agreement_number="52172";
-     supplier_item_number="SI-18";
-     supplier_contact_name="Gasol,Jim";
-     supplier_name= "Office Depot";
-     supplier_site_name= "OD US1";
-     price="7.99";
-	}
-	
-				   
-var orderxmlpayload= '<createRequisition>'+
-         '<interfaceSourceCode>UK_F2F_Bot</interfaceSourceCode>'+
-         '<requisitioningBUName>US1BusinessUnit</requisitioningBUName>'+
-         '<groupBy>NONE</groupBy>'+
-         '<maximumBatchSize>2500</maximumBatchSize>'+
-         '<errorProcessingLevel>ALL</errorProcessingLevel>'+
-         '<purchaseRequestPayload>'+
-            '<Description>Ladder with Right Handrail</Description>'+
-            '<ApproverEmail>fap1297-casey.brown@oracleads.com</ApproverEmail>'+
-            '<DocumentStatusCode>APPROVED</DocumentStatusCode>'+
-            '<PreparerEmail>fap1297-calvin.roth@oracleads.com</PreparerEmail>'+
-            '<RequisitioningBUName>US1 Business Unit</RequisitioningBUName>'+
-            '<ExternallyManagedFlag>FALSE</ExternallyManagedFlag>'+
-            '<PurchaseRequestInputReqLineInterface>'+
-               '<CategoryName>Pens and Pencils</CategoryName>'+
-               '<CurrencyCode>USD</CurrencyCode>'+
-               '<DeliverToLocationCode>Seattle</DeliverToLocationCode>'+
-               '<DeliverToOrganizationCode>001</DeliverToOrganizationCode>'+
-               '<DestinationTypeCode>EXPENSE</DestinationTypeCode>'+
-               '<ItemDescription>Round Stic Ball Point Pen, Fine, Blue</ItemDescription>'+
-               '<LineType>Goods</LineType>'+
-               '<ProcurementBUName>US1 Business Unit</ProcurementBUName>'+
-               '<Quantity>1</Quantity>'+
-               '<SourceAgreementNumber>52172</SourceAgreementNumber>'+
-               '<SupplierItemNumber>BIC20130</SupplierItemNumber>'+
-               '<SupplierContactName>Gasol, Jim</SupplierContactName>'+
-               '<SupplierName>Office Depot</SupplierName>'+
-               '<SupplierSiteName>OD US1</SupplierSiteName>'+
-               '<RequestedDeliveryDate>2017-06-16</RequestedDeliveryDate>'+
-               '<Price currencyCode="USD">2</Price>'+
-               '<UnitOfMeasure>Ea</UnitOfMeasure>'+
-               '<PurchaseRequestInputReqDistInterface>'+
-                  '<ChargeAccountId>300000047301445</ChargeAccountId>'+
-                  '<Percent>100</Percent>'+
-               '</PurchaseRequestInputReqDistInterface>'+
-            '</PurchaseRequestInputReqLineInterface>'+
-         '</purchaseRequestPayload>'+
-      '</createRequisition>';
-
+	  speech='lets see we have Solar Panel 0605 in stock with us. Should I go ahead and place the order for you?';
+	  
+	  if(order_yes==='Yes'){
+	  
+	  speech='should i get it delivered at your office address?';
+	  
+	  if(order_yes==='Yes'){
+	  
+	  speech='Please confirm your office address. Is it Blumberger Damm 2, 12683 Berlin?'
+	  
+	  if(order_yes==='Yes'){
+	  
+	  speech='Thank you for ordering with us. Your order OD507295 for Solar panel 0605 has been placed and is being processed. You can expect delivery by 17th July 2017.'
+	  }else{
+	  speech='Thank you for shopping with us.';
+	  }
+	  }else{
+	  speech='Thank you for shopping with us.';
+	  }
 	  
 	  
- 
-	var xmltojsonpayload= " ";  
-	
-	
-
-
-parsestring(orderxmlpayload, function (err, result) {
-    console.dir(result);
-	xmltojsonpayload=result;
-	
-});
-
-console.log("Request Payload is: "+JSON.stringify(xmltojsonpayload));
-
+	  }else{
+	  speech='Thank you for shopping with us.';
+	  }
 	  
-speech="Thank you for ordering Solar Panel with us. It will be delivered to by 15th july 2017."
+	  
 	
-	var options = {
-		headers: {
-         'Content-Type':'application/json;charset="UTF-8"'
-    },
-	method:"POST"
-	};
- 
-console.info('Do the SOAP call');
-
-soap.createClient(url,options,function(err, client){
-	
-	if(!err){
-		console.log("Setting security");
-   client.setSecurity(new soap.BasicAuthSecurity('calvin.roth', 'wxI69587'));
-  // The Client now has all the methods of the WSDL. Use it to create a new order by feeding it the JSON Payload
-  console.log('Calling Webservice');
-  client.createRequisition(xmltojsonpayload, function(err, result, body) {
-	  if(!err){
-	  console.log(body);
-	   parsestring(body, function(err, result){
-    // Get The Result From The Soap API and Parse it to JSON
-	if(!err){
-    var requestResult = result['SOAP-ENV:Envelope']['SOAP-ENV:Body'][0].createRequisitionResponse[0].return[0];
-    console.log(requestResult);
-	speech="Thank you for using requisition BOt. Your Id is"+requestResult;
 	}else{
-		console.log("There was some error in generating id."+err.message);
-		console.log(err.stack);
-		speech="There was some error in generating id.";
-  }
-	  });
-	  }
-	  else{
-		  console.log("There was some error in registering data  "+err.message);
-		  console.log(err.stack);
-		  speech="There was some error in registering data";
-	  }
-	});
-   }else{
-	   console.log(err.message);
-   }
-   
-});
+	speech='Thank you for shopping with us.';
+	}
+
 return res.json({
         speech: speech,
         displayText: speech,
         source: 'webhook-echo-sample'
 });
-
-}
 
 });
 
