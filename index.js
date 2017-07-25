d121ff6007b3e07036391fe2cdd656ccd1731db6
aashikhurana <aashi.khurana@gmail.com>
2017-07-25 13:28:49 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -22,21 +22,93 @@ var json_body_parser = bodyParser.json();
 
 
 restService.post('/echo', json_body_parser, function(req, res) {
+
+
 	console.log("Inside Web service call");
 	
 	
 	console.log(JSON.stringify(req.body.result));
-    var od_confirm=req.body.result.parameters.Order_confirm;
 	
+    var type_order=req.body.result.parameters.selection_type;
+	var item_order=req.body.result.parameters.Items;
+	var pen_color=req.body.result.parameters.pen_color;
+	var notebooktype=req.body.result.parameters.type_notebook;
+	var pap_conf=req.body.result.parameters.Paper_confirm;
+	var datetime = addDays(new Date(), 7);
+	console.log("Today's date:"+datetime);
 	
-	console.log("item confirmation is:"+od_confirm);
-	var speech="";
-	if(od_confirm==='Yes'){
-		console.log("Inside if block");
-	 speech="Please select an activity from below: 1. Order Status 2. Place Order";
+	if(type_order==='New order'){
+	 speech="Please choose one of the items from Pen,Paper,Stapler,Notebook";
+	}else{
+	   speech="Input is not valid. Please select from place a new order or Check Status.";
+	}
+	
+	if(type_order==='Check Status'){
+	 speech="Please tell me your Order ID";
+	}else{
+	   speech="Input is not valid. Please select from place a new order or Check Status.";
+	}
+	
+	if(item_order==='Pen'){
+	  
+	  speech="We have blue and black pens with us which one would you like to place order for."
+	  
+	  
+	}else{
+	   speech="Input is not valid. Please Chose one of the items from Pen,Paper,Stapler,Notebook";
+	}
+	
+	if(item_order==='Paper'){
+	  speech="Currently A4 Size paper is available should I go ahead and place order for you?";
 	}
 	else{
-	      speech="Thank You and Have a Nice Day Robert.";
+	 speech="Input is not valid. Please Chose one of the items from Pen,Paper,Stapler,Notebook";
+	}
+	
+	if(item_order==='notebook'){
+	  speech="We have hardbound and Spiral notebook which one would you like to place order for?";
+	}
+	else{
+	 speech="Input is not valid. Please Chose one of the items from Pen,Paper,Stapler,Notebook";
+	}
+	
+	if(item_order==='Stapler'){
+	  speech="Your order with ID 5075 has been confirmed and will be delivered to you by "+datetime;
+	}
+	else{
+	 speech="Input is not valid. Please Chose one of the items from Pen,Paper,Stapler,Notebook";
+	}
+	
+	if(pen_color==='Black'){
+	  speech="Your order with ID 7865 has been confirmed and will be delivered to you by "+datetime;
+	}
+	else{
+	 speech="Sorry order for "+pen_color +" pen could not be placed as currently it is not available. Would you like to place a new order or exit?";
+	}
+	
+	if(pen_color==='Blue'){
+	  speech="Your order with ID 91123 has been confirmed and will be delivered to you by "+datetime;
+	}
+	else{
+	  speech="Sorry order for "+pen_color +" pen could not be placed as currently it is not available. Would you like to place a new order or exit?";
+	}
+	
+	if(notebooktype==='hardbound'){
+	speech="Your order with ID 5421 has been confirmed and will be delivered to you by "+datetime;
+	}
+	else{
+	  speech="Sorry order for "+notebooktype +" notebook could not be placed as currently it is not available. Would you like to place a new order or exit?";
+	}
+	
+	if(notebooktype==='Spiral'){
+	speech="Your order with ID 7865 has been confirmed and will be delivered to you by "+datetime;
+	}
+	else{
+	 speech="Sorry order for "+notebooktype +" notebook could not be placed as currently it is not available. Would you like to place a new order or exit?";
+	}
+	
+	if(pap_conf==='yes'){
+		speech="Your order with ID 8954 has been confirmed and will be delivered to you by "+datetime;
 	}
 	  
 	  	 return res.json({
@@ -45,11 +117,12 @@ restService.post('/echo', json_body_parser, function(req, res) {
         source: 'webhook-echo-sample'
 });
 	  
-	  
+	  });
 
+function addDays(theDate, days) {
+    return new Date(theDate.getTime() + days*24*60*60*1000);
+}
 
-
-});
 
 
 restService.listen((process.env.PORT || 5000), function() {
